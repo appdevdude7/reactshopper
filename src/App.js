c70e9672a601199aa67d65eae8f7daaffa269828
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-02 12:19:36 -0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SinglePage from './components/SinglePage';
+import Home from './components/Home';
+
 function App() {
   return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/product">Product</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/product">
+            <SinglePage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+  /* return (
     <div className="App">
       <header className="App-header">
         <p>
@@ -10,7 +46,7 @@ function App() {
         </p>
       </header>
     </div>
-  );
+  ); */
 }
 
 export default App;
