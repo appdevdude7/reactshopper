f79f9b4c5c9eb06e3a4ea8102f82b89fdb59c242
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-02 12:33:42 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,20 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Link class="navbar-brand" to="/">Navbar</Link>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link align-right" to="/product">Link</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+        {/* <nav>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -23,7 +36,7 @@ function App() {
               <Link to="/product">Product</Link>
             </li>
           </ul>
-        </nav>
+        </nav> */}
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
